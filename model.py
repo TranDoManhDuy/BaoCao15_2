b83498909690376fa88b968d0e49ae6bd5704f55
TranDoManhDuy <178302863+TranDoManhDuy@users.noreply.github.com>
2025-02-12 20:50:16 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 
+# Kiến trúc mạng.
 architecture_config = [
+    # (kernel_size, out_channels, stride, padding)
     (7, 64, 2, 3),
-    "M",
+    "M", # M maxplool(kernel_size = 2, stride = 2)
     (3, 192, 1, 1),
     "M", 
     (1, 128, 1, 0),
@@ -12,7 +14,7 @@ architecture_config = [
     (3, 512, 1, 1),
     "M",
     # List
-[(1, 256, 1, 0), (3, 512, 1, 1), 4], # (), (), (), n trong đó mỗi
+[(1, 256, 1, 0), (3, 512, 1, 1), 4], # (), (), n trong đó mỗi
     # ngoặc là 1 lớp và n là số lần lặp lại.
     (1, 512, 1, 0),
     (3, 1024, 1, 1),
@@ -27,10 +29,12 @@ architecture_config = [
 
 # xây dựng lên một nơ ron mạng.
 class CNNBlock(nn.Module):
+    # in_channels và out_channels xác định đầu vào, đầu ra của lớp Conv2D trọng mạng nơ ron tích chập.
     def __init__(self, in_channels, out_channels, **kwargs):
         super(CNNBlock, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
-        self.batchnorm = nn.BatchNorm2d(out_channels) # vốn dĩ ko nằm trong yoloV1, được áp dụng từ yolov2
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs) # kwargs chứa các tham số bổ sung.
+        self.batchnorm = nn.BatchNorm2d(out_channels) # vốn dĩ ko nằm trong yoloV1, được áp dụng từ yolov2. Chính vì sử dụng batchnorm nên ko cần trọng số bias
+        # hàm kích hoạt.
         self.leakyrelu = nn.LeakyReLU(0.1)
         
     # trình từ đi qua nơ ron.
@@ -43,7 +47,9 @@ class Yolov1(nn.Module):
         super(Yolov1, self).__init__()
         self.architecture = architecture_config
         self.in_channels = in_channels
+        # darknet là thành phần chính của mô hình, trích xuất đặc trưng từ đầu vào.
         self.darknet = self._create_conv_layers(self.architecture)
+        # lớp fully connection
         self.fcs = self._create_fcs(**kwargs)
     
     def forward(self, x):
@@ -85,18 +91,17 @@ class Yolov1(nn.Module):
                     ]
                     
                     in_channels = conv2[1]
+        # là một container của pytorh, gói các layer lại với nhau và thực hiện một cách tuần tự.
         return nn.Sequential(*layers)
-    
+
+    # Tạo ra lớp fully connected để xử lý các đặc trưng đã được trích xuất, từ đố đưa ra dự đoán.
     def _create_fcs(self, split_size, num_boxes, num_classes):
         return nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(1024 * split_size * split_size, 4096),
-            nn.Dropout(0.0),
-            nn.LeakyReLU(0.1),
+            nn.Flatten(), # làm phẳng tensor
+            nn.Linear(1024 * split_size * split_size, 4096), # kết nối đầu ra của flatten với 4096 node.
+            # giảm số lượng node giúp mô hình tập trung vào các đặc trưng hơn, giảm lượng dữ liệu tránh overfiting.
+            # tăng khả năng tổng quát.
+            nn.Dropout(0.0), # là một kí thuật regulazization nhằm giảm overfiting
+            nn.LeakyReLU(0.1), # hàm kích hoạt, biến thể của ReLu loại bỏ nguy cơ chết nơ ron
             nn.Linear(4096, split_size * split_size * (num_classes + num_boxes * 5)), # (S, S, 30) where C + B + 5 = 30
         )
-def test(S = 7, B = 2, C = 20):
-    model = Yolov1(split_size = S, num_boxes = B, num_classes = C)
-    x = torch.randn(2, 3, 448, 448)
-    print(model(x).shape)
-test()
